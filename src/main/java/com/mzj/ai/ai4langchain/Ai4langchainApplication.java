e2f4c56beed727f067c3274014984bd46fb020f8
syang <syang_010@163.com>
2025-04-05 12:21:47 +0800

    初始化项目

---
+++ src/main/java/com/mzj/ai/ai4langchain/Ai4langchainApplication.java
@@ -0,0 +1,13 @@
+package com.mzj.ai.ai4langchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ai4langchainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ai4langchainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mzj/ai/ai4langchain/Ai4langchainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mzj.ai.ai4langchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ai4langchainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
